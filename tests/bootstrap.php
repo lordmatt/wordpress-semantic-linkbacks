a164a3ddb11dc58a58bb19477732ebacb4e85915
Matthias Pfefferle <pfefferle@gmail.com>
2022-04-30 19:40:52 +0200

    updage bookstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,21 +1,40 @@
 <?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package semantic-linkbacks
+ */
 
-error_reporting( E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING );
 
-define( 'WP_DEBUG', false );
-
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+// Forward custom PHPUnit Polyfills configuration to PHPUnit bootstrap file.
+$_phpunit_polyfills_path = getenv( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' );
+if ( false !== $_phpunit_polyfills_path ) {
+	define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', $_phpunit_polyfills_path );
 }
 
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	exit( 1 );
+}
+
+// Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
+require_once dirname( dirname( __FILE__ ) ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/semantic-linkbacks.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
-
-error_reporting( E_ALL & ~E_NOTICE & ~E_STRICT & ~E_WARNING );
