92197caeebb6f8dbb5727688632da848f1cd451b
Matthias Pfefferle <pfefferle@gmail.com>
2022-04-30 19:23:50 +0200

    fix tests

--- tests/test-rendering.php
+++ tests/test-rendering.php
@@ -1,32 +1,33 @@
 <?php
 class RenderingTest extends WP_UnitTestCase {
-	public function setUp() {
+	protected function setUp() {
 		parent::setUp();
 		update_option( 'semantic_linkbacks_facepiles_fold_limit', 2 );
 		add_filter(
-			'comment_flood_filter', function() {
+			'comment_flood_filter',
+			function() {
 				return false;
 			}
 		);
 		add_filter(
-			'get_avatar_comment_types', function( $types ) {
+			'get_avatar_comment_types',
+			function( $types ) {
 				$types[] = 'webmention';
 				return $types;
 			}
 		);
-
 	}
 
 	public function make_comments( $num, $semantic_linkbacks_type = 'like' ) {
 		$comments = array();
-		$post_id = wp_insert_post(['post_title'=>'Some Post']);
+		$post_id  = wp_insert_post( array( 'post_title' => 'Some Post' ) );
 		for ( $i = 0; $i < $num; $i++ ) {
 			$id = wp_new_comment(
 				array(
 					'comment_author_url' => 'http://example.com/person' . $i,
 					'comment_author'     => 'Person ' . $i,
 					'comment_type'       => 'webmention',
-					'comment_post_ID'    => $post_id
+					'comment_post_ID'    => $post_id,
 				)
 			);
 			add_comment_meta( $id, 'semantic_linkbacks_type', $semantic_linkbacks_type );
@@ -39,7 +40,8 @@ class RenderingTest extends WP_UnitTestCase {
 	public function test_facepile_markup() {
 		$comments = $this->make_comments( 1 );
 		$this->assertStringMatchesFormat(
-			'<ul class="mention-list linkback-mention"><li class="webmention even thread-even depth-1 linkback-mention-single u-like h-cite" id="comment-2"><span class="p-author h-card"><a class="u-url" title="Person 0 liked this Post on example.com." href="http://example.com/person0"><img alt=\'\' src=\'http://example.com/photo\' srcset=\'http://example.com/photo 2x\' class=\'avatar avatar-64 photo avatar-default u-photo avatar-semantic-linkbacks\' height=\'64\' width=\'64\' loading=\'lazy\'/></a><span class="hide-name p-name">Person 0</span></span><a class="u-url" href=""></a></li></ul>', list_linkbacks( array( 'echo' => false ), $comments )
+			'<ul class="mention-list linkback-mention"><li class="webmention even thread-even depth-1 linkback-mention-single u-like h-cite" id="comment-2"><span class="p-author h-card"><a class="u-url" title="Person 0 liked this Post on example.com." href="http://example.com/person0"><img alt=\'\' src=\'http://example.com/photo\' srcset=\'http://example.com/photo 2x\' class=\'avatar avatar-64 photo avatar-default u-photo avatar-semantic-linkbacks\' height=\'64\' width=\'64\' loading=\'lazy\'/></a><span class="hide-name p-name">Person 0</span></span><a class="u-url" href=""></a></li></ul>',
+			list_linkbacks( array( 'echo' => false ), $comments )
 		);
 	}
 
@@ -88,7 +90,8 @@ class RenderingTest extends WP_UnitTestCase {
 		$this->assertStringMatchesFormat(
 			'<div class="reactions">
 	<h3>Reacjis</h3>
-	<ul class="mention-list linkback-reacji"><li class="comment even thread-even depth-1 linkback-reacji-single h-cite" id="comment-%d"><span class="p-author h-card"><a class="u-url" title="Person 😢 on example.com." href="http://example.com/person"><img alt=\'\' src=\'http://example.com/photo\' srcset=\'http://example.com/photo 2x\' class=\'avatar avatar-64 photo avatar-default u-photo avatar-semantic-linkbacks\' height=\'64\' width=\'64\' loading=\'lazy\'/> <span class="emoji-overlay">😢</span></a><span class="hide-name p-name">Person</span></span><a class="u-url" href=""></a></li></ul></div>', trim( $html )
+	<ul class="mention-list linkback-reacji"><li class="comment even thread-even depth-1 linkback-reacji-single h-cite" id="comment-%d"><span class="p-author h-card"><a class="u-url" title="Person 😢 on example.com." href="http://example.com/person"><img alt=\'\' src=\'http://example.com/photo\' srcset=\'http://example.com/photo 2x\' class=\'avatar avatar-64 photo avatar-default u-photo avatar-semantic-linkbacks\' height=\'64\' width=\'64\' loading=\'lazy\'/> <span class="emoji-overlay">😢</span></a><span class="hide-name p-name">Person</span></span><a class="u-url" href=""></a></li></ul></div>',
+			trim( $html )
 		);
 	}
 }
